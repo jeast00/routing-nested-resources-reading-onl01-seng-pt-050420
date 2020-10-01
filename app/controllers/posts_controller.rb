037d6288574d004200e67f3024de4f23f4b8b100
Jonathan East <jonathan_east@ymail.com>
2020-10-01 15:09:14 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,9 @@ class PostsController < ApplicationController
   def index
     if params[:author_id]
       @posts = Author.find(;params[:author_id]).posts
+    else
+      @posts = Post.all
     end
-    @posts = Post.all
   end
 
   def show
